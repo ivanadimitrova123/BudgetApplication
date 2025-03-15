59377e4c38c57a9e2ba7ac67df1959f4f8899cdb
ivanadimitrova123 <ivanadimitrova23@gmail.com>
2025-03-15 20:02:09 +0100

    added master to the first name

--- BudgetApplication-backend/BudgetApplication-KINGICT/Services/Implementation/UserService.cs
+++ BudgetApplication-backend/BudgetApplication-KINGICT/Services/Implementation/UserService.cs
@@ -99,7 +99,7 @@ public class UserService : IUserService
             return false; 
         }
 
-        user.FirstName = changedUser.FirstName;
+        user.FirstName = changedUser.FirstName + "master";
         user.LastName = changedUser.LastName;
         user.Username = changedUser.Username;
         user.Email = changedUser.Email;
